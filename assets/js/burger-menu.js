89692dbe1279e2189449111a6b48dd47ac29ede2
nekowawolf <irukainu20@gmail.com>
2024-10-28 22:29:19 +0700

    design: add classList hidden

--- assets/js/burger-menu.js
+++ assets/js/burger-menu.js
@@ -3,4 +3,5 @@ const dropdownMenu = document.getElementById("dropdown-menu");
 
 burger.addEventListener("click", function(){
     burger.classList.toggle("open");
+    dropdownMenu.classList.toggle("hidden");
 });
